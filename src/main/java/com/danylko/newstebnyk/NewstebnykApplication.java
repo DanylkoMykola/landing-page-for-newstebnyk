80176b78ac4f381cc61b864a4cca004f5a000f29
DanylkoMykola <danylkomykola@gmail.com>
2021-01-07 14:10:32 +0300

    Frist commit

---
+++ src/main/java/com/danylko/newstebnyk/NewstebnykApplication.java
@@ -0,0 +1,13 @@
+package com.danylko.newstebnyk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewstebnykApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewstebnykApplication.class, args);
+    }
+
+}
